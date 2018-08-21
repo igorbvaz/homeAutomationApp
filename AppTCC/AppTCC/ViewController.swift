c32b5e2de5d375d60a25051f6977df03c3e6958b
Igor Vaz <igorvaz@slt-000169.solutis.net.br>
2018-08-21 11:06:28 -0300

    Blank project

---
+++ AppTCC/AppTCC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppTCC
+//
+//  Created by Igor Vaz on 21/08/18.
+//  Copyright © 2018 Igor Vaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
